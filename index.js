eb9056f0f0048de1fa6044c1f0a828baf42458d5
Donny Cheng <donccl@gmail.com>
2018-04-18 15:24:19 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express();
+const http = require('http').Server(app);
+
+app.get('/randomNumber', (req, res)=>{
+  res.send(`${Math.random()}`);
+})
+
+http.listen(8000, ()=>{
+  console.log('app running...');
+});
+
+module.exports = app;
